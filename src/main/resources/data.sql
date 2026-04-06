faf1780229a01c23e63bd376c59b815226ef7d2b
Hariprasad <hariprasadjnv963@gmail.com>
2026-04-06 16:52:55 +0530

    chore: add SQL data script for default RBAC test users

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (username, password, role, active)
+VALUES ('admin', '{noop}admin123', 'ADMIN', true),
+       ('analyst', '{noop}analyst123', 'ANALYST', true),
+       ('viewer', '{noop}viewer123', 'VIEWER', true);
