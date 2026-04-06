dc2ff71e3ff1dce268c46cf6c61970c7fc6b9b43
Hariprasad <hariprasadjnv963@gmail.com>
2026-04-06 16:50:04 +0530

    feat: implement Spring Security with RBAC for ADMIN, ANALYST, and VIEWER

---
+++ src/main/java/com/finance/dashboard/config/SecurityConfig.java
@@ -0,0 +1,48 @@
+package com.finance.dashboard.config;
+
+import com.finance.dashboard.service.CustomUserDetailsService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .userDetailsService(userDetailsService)
+                .authorizeHttpRequests(auth -> auth
+
+                        .requestMatchers(HttpMethod.POST, "/records").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.PUT, "/records/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/records/**").hasRole("ADMIN")
+
+                        .requestMatchers(HttpMethod.GET, "/records/filter").hasAnyRole("ADMIN", "ANALYST")
+                        .requestMatchers(HttpMethod.GET, "/dashboard").hasAnyRole("ADMIN", "ANALYST")
+
+                        .requestMatchers(HttpMethod.GET, "/records").hasAnyRole("ADMIN", "ANALYST", "VIEWER")
+
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+
+    private final CustomUserDetailsService userDetailsService;
+
+    public SecurityConfig(CustomUserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+}
